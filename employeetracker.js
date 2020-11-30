2d5f0079f47fb0ccb754505fb2a11402d9bd4136
Thomas Rager <tomrager@yahoo.com>
2020-11-30 15:05:32 -0500

    View and Update Functions are working? Not sure if it's to the requirement of the assignment

--- employeetracker.js
+++ employeetracker.js
@@ -129,15 +129,15 @@ const view=()=>{
                     const depts=new Department();
                     connection.query(depts.view(),(err,res)=>{
                         if (err) throw err;
-                        cTable(res);
+                        console.table(res);
                     })
                     break;
-                case 'Role':
+                case 'Employee Roles':
                     console.log('Current employee roles');
                     const jobs=new Role();
                     connection.query(jobs.view(),(err,res)=>{
                         if (err) throw err;
-                        cTable(res);
+                        console.table(res);
                     })
                     break;
                 case 'Employees':
@@ -145,119 +145,73 @@ const view=()=>{
                     const employees=new Employee();
                     connection.query(employees.view(),(err,res)=>{
                         if (err) throw err;
-                        cTable(res);
+                        console.table(res);
                     })
                     break;
                 case 'Go Back':
                     dbSearch();
                     break;
             }
+            dbSearch();
         })
 }
 
 //Function to update an employee role
 const update=()=>{
-    const emp_Roles=connection.query('SELECT title from Job',(err,res)=>{
+    connection.query('SELECT title from Job',(err,res)=>{
         if (err) throw err;
         else{
-        return (res)=>[...res,'Go Back'];
-        };
-    });
-    inquirer
-        .prompt({
-            name: 'update',
-            type: 'list',
-            message: 'What employee role would you like to update?',
-            choices: [...emp_Roles]
-        })
-        .then((select)=>{
-            if (select.update==='Go Back'){
-                dbSearch();
+            var emp_Roles=[];
+            for(var i=0; i<res.length; i++){
+                emp_Roles.push(res[i].title);
             }
-            else{
-                connection.query('SELECT title,salary,department_id FROM Job WHERE title=?',{title:select.update},(err,res)=>{
-                    if (err) throw err;
-                    console.log('Current Role Information');
-                    cTable(res);
+            inquirer
+                .prompt({
+                    name: 'update',
+                    type: 'list',
+                    message: 'What employee role would you like to update?',
+                    choices: [...emp_Roles,'Go Back']
                 })
-                inquirer
-                    .prompt([
-                        {
-                        name: 'jobTitle',
-                        type: 'input',
-                        message: 'Enter current job title or the new job title you would like to give this role'
-                        },
-                        {  
-                        name: 'jobSalary',
-                        type: 'input',
-                        message: 'Enter the current salary of this role or the new salary for this role'
-                        },
-                        {
-                        name: 'jobDeptID',
-                        type: 'input',
-                        message: 'Enter the current department ID for this role or the new department ID for this role'
-                        }
-                    ])
-                    .then((data)=>{
-                        const updateQuery='UPDATE Job SET title='+data.jobTitle+
-                                        ' SET salary='+data.jobSalary+
-                                        ' SET department_id='+data.jobDeptID+
-                                        ' WHERE title='+select.update;
-                        connection.query(updateQuery,(err,current)=>{
-                        if (err) throw err;
-                        console.log('Role updated successfullly!');
-                        cTable(current);
+                .then((select)=>{
+                    if (select.update==='Go Back'){
+                        dbSearch();
+                    }
+                    else{
+                        connection.query('SELECT title,salary FROM Job WHERE ?',[{title:select.update}],(err,res)=>{
+                            if (err) throw err;
+                            else {
+                                console.log('Current Role Information');
+                                console.table(res);
+                                inquirer
+                                    .prompt([
+                                        {
+                                        name: 'jobTitle',
+                                        type: 'input',
+                                        message: 'Enter current job title or the new job title you would like to give this role'
+                                        },
+                                        {  
+                                        name: 'jobSalary',
+                                        type: 'input',
+                                        message: 'Enter the current salary of this role or the new salary for this role'
+                                        }
+                                    ])
+                                    .then((data)=>{
+                                        const updateQuery='UPDATE Job SET title="'+data.jobTitle+'",'+
+                                                        'salary='+data.jobSalary+
+                                                        ' WHERE title="'+select.update+'"';
+                                        connection.query(updateQuery,(err,current)=>{
+                                            if (err) throw err;
+                                            else{
+                                                console.log('Role updated successfullly!');
+                                                console.table(current);
+                                                dbSearch();
+                                            }
+                                        })
+                                    })
+                            }
                         })
-                    })
-            } 
-        })
-};
-
-// //Function to delete a department, role, or employee
-// const remove=()=>{
-//     inquirer
-//         .prompt({
-//             name: 'remove',
-//             type: 'list',
-//             message: 'What would you like to delete?',
-//             choices: [
-//                 'Department',
-//                 'Employee Role',
-//                 'Employee',
-//                 'Go Back'
-//             ]
-//         })
-//         .then((select)=>{
-//             switch(select.remove){
-//                 case 'Department':
-//                     department.remove();
-//                     break;
-//                 case 'Employee Role':
-//                     role.remove();
-//                     break;
-//                 case 'Employee':
-//                     employee.remove();
-//                     break;
-//                 case 'Go Back':
-//                     dbSearch();
-//                     break;
-//             }
-//         })
-// }
-
-// //Function to display the individual budget of each department
-// const budget=()=>{
-//     inquirer
-//         .prompt({
-//             name: 'budget',
-//             type: 'list',
-//             message: 'Select the department whose budget you would like to see',
-//             choices: [
-//                 //Use query to display all current departments and go back option
-//             ]
-//         })
-//         .then((select)=>{
-//                 //Use query to provide budget for the department chosen
-//         })
-
-// }
+                    }
+                })    
+        }        
+    })
+}
